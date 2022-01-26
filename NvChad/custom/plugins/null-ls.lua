25ac26ba8b5b8681ecf2b926b35176d806a455be
Jacob Biehler <biehlerj@protonmail.com>
2022-01-25 18:52:47 -0800

    Fixing null-ls config

--- NvChad/custom/plugins/null-ls.lua
+++ NvChad/custom/plugins/null-ls.lua
@@ -46,16 +46,19 @@ local sources = {
 }
 
 local M = {}
-M.setup = function(on_attach)
-   on_attach = function(client)
-      if client.resolved_capabilities.document_formatting then
-         vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
-      end
-   end
-   null_ls.config {
+
+M.setup = function()
+   null_ls.setup {
+      debug = true,
       sources = sources,
+
+      -- format on save
+      on_attach = function(client)
+         if client.resolved_capabilities.document_formatting then
+            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+         end
+      end,
    }
-   require("lspconfig")["null-ls"].setup { on_attach = on_attach }
 end
 
 return M
