dfc52bbc9f3832fd286bacaf909ddbb5e6368384
Jacob Biehler <jacob@biehlerj.xyz>
2022-06-21 21:44:10 -0700

    Adding bootstrap script to clone all my sourcehut repos

---
+++ .config/yadm/bootstrap.d/clone_sourcehut_repos.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Clones all repos from sourcehut
+repos=(go-cli-intro go-logging-intro go-grpc-intro advent-of-code Awesome-CV algos-datastructures astronvim_config dotfiles biehlerj.xyz java-development-bootcamp java-bootcamp-resources vimwiki introducing-go private-scripts obsidian)
+
+# List all subdirectories in the current directory
+for repo in "${repos[@]}"; do
+    # Clone
+    git clone "git@git.sr.ht:~biehlerj/$repo"
+done
