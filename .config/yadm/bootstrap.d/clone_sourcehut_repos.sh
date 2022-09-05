033899c0184392e6d2de5294c6e1bb69b9028257
Jacob Biehler <jacob@biehlerj.xyz>
2022-09-05 00:23:49 -0700

    Fixing bootstrap configs and adding dnf completions

--- .config/yadm/bootstrap.d/clone_sourcehut_repos.sh
+++ .config/yadm/bootstrap.d/clone_sourcehut_repos.sh
@@ -2,9 +2,9 @@
 # Clones all repos from sourcehut
 repos=(go-cli-intro go-logging-intro go-grpc-intro advent-of-code Awesome-CV algos-datastructures astronvim_config dotfiles biehlerj.xyz java-development-bootcamp java-bootcamp-resources vimwiki introducing-go private-scripts pdf_compiler)
 
-mkdir -p ~/dev/git.sr.ht/biehlerj/
+mkdir -p "$HOME/dev/git.sr.ht/biehlerj/"
 # List all subdirectories in the current directory
 for repo in "${repos[@]}"; do
     # Clone
-    git clone "git@git.sr.ht:~biehlerj/$repo" "~/dev/git.sr.ht/biehlerj/$repo"
+    git clone "git@git.sr.ht:~biehlerj/$repo" "$HOME/dev/git.sr.ht/biehlerj/$repo"
 done
